8bed443ab541a430d2872c4d44462d3ed15ef7cc
Carl Buena <carlbuena94@gmail.com>
2016-09-22 14:59:22 +1000

    22/9 Create Food Order

--- app/src/main/java/ps1623/nalla_ruchi/CreateOrder.java
+++ app/src/main/java/ps1623/nalla_ruchi/CreateOrder.java
@@ -1,6 +1,6 @@
 package ps1623.nalla_ruchi;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -18,17 +18,19 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+
 /**
  * Created by Prazad Silva on 16/9/2016.
  */
-public class CreateBooking extends AppCompatActivity implements Spinner.OnItemSelectedListener {
-
+public class CreateOrder extends AppCompatActivity implements Spinner.OnItemSelectedListener {
 
     //Declaring an Spinner
-    private Spinner spinner;
+    private Spinner cookOrderSpinner;
+    private Spinner FoodSpinner;
 
     //An ArrayList for Spinner Items
     private ArrayList<String> cooks;
+    private ArrayList<String> food;
 
     //JSON Array
     private JSONArray result;
@@ -36,27 +38,42 @@ public class CreateBooking extends AppCompatActivity implements Spinner.OnItemSe
     //TextViews to display details
     private TextView textViewFirstName;
     private TextView textViewSurname;
-    private TextView textViewState;
+    private TextView textViewAddress;
+
+    private TextView textViewPrice;
+    private TextView textViewDescription;
+    private TextView textViewEthnicity;
+    private TextView textViewType;
+    private TextView textViewDishType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.create_booking);
+        setContentView(R.layout.create_order);
 
         //Initializing the ArrayList
         cooks = new ArrayList<String>();
+        food = new ArrayList<String>();
 
         //Initializing Spinner
-        spinner = (Spinner) findViewById(R.id.spinner);
+        cookOrderSpinner = (Spinner) findViewById(R.id.cookOrderSpinner);
+        FoodSpinner = (Spinner) findViewById(R.id.FoodSpinner);
 
         //Adding an Item Selected Listener to our Spinner
-        //As we have implemented the class Spinner.OnItemSelectedListener to this class iteself we are passing this to setOnItemSelectedListener
-        spinner.setOnItemSelectedListener(this);
+        //As we have implemented the class Spinner.OnItemSelectedListener to this class itself we are passing this to setOnItemSelectedListener
+        cookOrderSpinner.setOnItemSelectedListener(this);
+        FoodSpinner.setOnItemSelectedListener(this);
 
         //Initializing TextViews
         textViewFirstName = (TextView) findViewById(R.id.textViewFirstName);
         textViewSurname = (TextView) findViewById(R.id.textViewSurname);
-        textViewState = (TextView) findViewById(R.id.textViewState);
+        textViewAddress = (TextView) findViewById(R.id.textViewAddress);
+
+        textViewPrice = (TextView) findViewById(R.id.textViewPrice);
+        textViewDescription = (TextView) findViewById(R.id.textViewDescription);
+        textViewEthnicity = (TextView) findViewById(R.id.textViewEthnicity);
+        textViewType = (TextView) findViewById(R.id.textViewType);
+        textViewDishType = (TextView) findViewById(R.id.textViewDishType);
 
         //This method will fetch the data from the URL
         getData();
@@ -104,7 +121,7 @@ public class CreateBooking extends AppCompatActivity implements Spinner.OnItemSe
                 //Getting json object
                 JSONObject json = j.getJSONObject(i);
 
-                //Adding the name of the student to array list
+                //Adding the name of the cook to array list
                 cooks.add(json.getString(Config.TAG_COOK_FIRSTNAME));
             } catch (JSONException e) {
                 e.printStackTrace();
@@ -112,7 +129,18 @@ public class CreateBooking extends AppCompatActivity implements Spinner.OnItemSe
         }
 
         //Setting adapter to show the items in the spinner
-        spinner.setAdapter(new ArrayAdapter<String>(CreateBooking.this, android.R.layout.simple_spinner_dropdown_item, cooks));
+        cookOrderSpinner.setAdapter(new ArrayAdapter<String>(CreateOrder.this, android.R.layout.simple_spinner_dropdown_item, cooks));
+    }
+
+    private String getCookID(int position){
+        String course="";
+        try {
+            JSONObject json = result.getJSONObject(position);
+            course = json.getString(Config.TAG_COOK_ID);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return course;
     }
 
     //Method to get cook name of a particular position
@@ -143,12 +171,74 @@ public class CreateBooking extends AppCompatActivity implements Spinner.OnItemSe
         return course;
     }
 
-    //Getting cook state
-    private String getCookState(int position){
+    //Getting cook address
+    private String getCookAddress(int position){
         String session="";
         try {
             JSONObject json = result.getJSONObject(position);
-            session = json.getString(Config.TAG_COOK_STATE);
+            session = json.getString(Config.TAG_COOK_ADDRESS);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return session;
+    }
+
+    //Get food details
+    private String getFoodID(int position){
+        String session="";
+        try {
+            JSONObject json = result.getJSONObject(position);
+            session = json.getString(Config.TAG_ID);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return session;
+    }
+    private String getFoodPrice(int position){
+        String session="";
+        try {
+            JSONObject json = result.getJSONObject(position);
+            session = json.getString(Config.TAG_PRICE);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return session;
+    }
+    private String getFoodDescription(int position){
+        String session="";
+        try {
+            JSONObject json = result.getJSONObject(position);
+            session = json.getString(Config.TAG_DES);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return session;
+    }
+    private String getFoodEthnicity(int position){
+        String session="";
+        try {
+            JSONObject json = result.getJSONObject(position);
+            session = json.getString(Config.TAG_ETH);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return session;
+    }
+    private String getFoodType(int position){
+        String session="";
+        try {
+            JSONObject json = result.getJSONObject(position);
+            session = json.getString(Config.TAG_TYPE);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return session;
+    }
+    private String getFoodDishType(int position){
+        String session="";
+        try {
+            JSONObject json = result.getJSONObject(position);
+            session = json.getString(Config.TAG_DISH);
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -160,9 +250,16 @@ public class CreateBooking extends AppCompatActivity implements Spinner.OnItemSe
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         //Setting the values to textviews for a selected item
+        textViewFirstName.setText(getCookID(position));
         textViewFirstName.setText(getCookName(position));
         textViewSurname.setText(getCookSurname(position));
-        textViewState.setText(getCookState(position));
+        textViewAddress.setText(getCookAddress(position));
+
+        textViewPrice.setText(getFoodPrice(position));
+        textViewDescription.setText(getFoodDescription(position));
+        textViewEthnicity.setText(getFoodEthnicity(position));
+        textViewType.setText(getFoodType(position));
+        textViewDishType.setText(getFoodDishType(position));
     }
 
     //When no item is selected this method would execute
@@ -170,35 +267,12 @@ public class CreateBooking extends AppCompatActivity implements Spinner.OnItemSe
     public void onNothingSelected(AdapterView<?> parent) {
         textViewFirstName.setText("");
         textViewSurname.setText("");
-        textViewState.setText("");
+        textViewAddress.setText("");
+
+        textViewPrice.setText("");
+        textViewDescription.setText("");
+        textViewEthnicity.setText("");
+        textViewType.setText("");
+        textViewDishType.setText("");
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
